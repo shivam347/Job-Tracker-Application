ec689bd65af93d7c13291e0fc24cc66a6855c930
Shivam <shivamfeb77@gmail.com>
2025-08-24 11:25:26 +0530

    created user model

--- src/main/java/com/java/model/User.java
+++ src/main/java/com/java/model/User.java
@@ -1,12 +1,92 @@
 package com.java.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
-    
+    /* Identity type means auto generate the primary key upon insertion in table */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Size(max = 50)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Size(max = 50)
+    @Column(name = "last_name")
+    private String lastName;
+
+    /* Column definition is mainly used
+    for custom sql data type, text is used 
+    for large text fields
+     */
+    @Column(name = "gmail_token", columnDefinition = "TEXT")
+    private String gmailToken;
+
+    @Column(name = "gmail_refresh_token", columnDefinition = "TEXT")
+    private String gmailRefreshToken;
+
+    @Column(name = "gmail_connected")
+    private Boolean gmailConnected = false;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    // constructors 
+
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public User(String email, String password, String firstName, String lastName){
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
     
 }
