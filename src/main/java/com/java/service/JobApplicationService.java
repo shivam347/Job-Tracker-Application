4ec80253aaf628d7a1191335d2c4f1e5620ea9eb
Shivam <shivamfeb77@gmail.com>
2025-08-28 15:36:01 +0530

    update jobApplication service layer

--- src/main/java/com/java/service/JobApplicationService.java
+++ src/main/java/com/java/service/JobApplicationService.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
